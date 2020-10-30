4551f63a17cb400dd86ac8a62976b40b76e5e970
Daniel Pavela <daniel.pavela@gmail.com>
2020-10-30 19:45:22 +1100

    Update script - check for metered connections + comments

--- src/main.sh
+++ src/main.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 
-if [ $(dunstify -A yes,Yes -A no,No "Download updates?" "In order to help you update faster") = "yes" ]
-then
-	pkexec pacman -Syuw
-	notify-send "Downloading Complete!" "Use pacman -Su to update your system"
+# Check if the connection is metered - Don't download updates on metered connections
+# https://developer.gnome.org/NetworkManager/stable/nm-dbus-types.html#NMMetered
+if [ "$(busctl get-property org.freedesktop.NetworkManager /org/freedesktop/NetworkManager org.freedesktop.NetworkManager Metered)" != "u 1" ]; then
+	# Ask user in notification
+	if [ $(dunstify -A yes,Yes -A no,No "Download updates?" "In order to help you update faster") = "yes" ]; then
+		# Run update command as root
+		pkexec pacman -Syuw
+		# Notify user
+		notify-send "Downloading Complete!" "Use pacman -Su to update your system"
+	fi
+else
+	echo "Metered network detected, exiting"
 fi
